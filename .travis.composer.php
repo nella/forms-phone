6808c24eaba963c6b3a2c4a46dd284194614a9d1
Patrik Votocek <patrik@votocek.cz>
2014-05-21 14:31:01 +0200

    travis: nette version switcher refactoring

--- .travis.composer.php
+++ .travis.composer.php
@@ -10,9 +10,10 @@ echo "Nette version " . $version . PHP_EOL;
 
 $file = __DIR__ . '/composer.json';
 $content = file_get_contents($file);
-$content = preg_replace(
-	'~"nette/nette": "([^"]+)"~',
-	'"nette/nette": "' . $version . '"',
-	$content
-);
+$composer  = json_decode($content, TRUE);
+if (!isset($composer['require']['nette/forms'])) {
+	exit(255);
+}
+$composer['require']['nette/forms'] = $version;
+$content = json_encode($composer);
 file_put_contents($file, $content);
